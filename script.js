78e4058cc1bd6b04e042d067c88dc46a73d003cf
emmanuelraob <emmanuelraob123@gmail.com>
2023-08-27 17:13:19 -0600

    First update
    Logo
    some index and code to pass the 3 different screens

---
+++ script.js
@@ -0,0 +1,27 @@
+function uno() {
+    let notConnectedDiv = document.getElementById('beforeStart');
+    let connectingDiv = document.getElementById('startedNotConnected');
+    let connectedDiv = document.getElementById('connected');
+
+    notConnectedDiv.style.display = 'block';
+    connectingDiv.style.display = 'none';
+    connectedDiv.style.display = 'none';
+}
+function dos() {
+    let notConnectedDiv = document.getElementById('beforeStart');
+    let connectingDiv = document.getElementById('startedNotConnected');
+    let connectedDiv = document.getElementById('connected');
+
+    notConnectedDiv.style.display = 'none';
+    connectingDiv.style.display = 'block';
+    connectedDiv.style.display = 'none';
+}
+function tres() {
+    let notConnectedDiv = document.getElementById('beforeStart');
+    let connectingDiv = document.getElementById('startedNotConnected');
+    let connectedDiv = document.getElementById('connected');
+
+    notConnectedDiv.style.display = 'none';
+    connectingDiv.style.display = 'none';
+    connectedDiv.style.display = 'block';
+}
